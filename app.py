569dbb7f6775a160e4b22881e34d926cc70897f2
SamTGoodson <samtgoodson@gmail.com>
2024-12-30 15:01:53 -0500

    add table

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import geopandas as gpd
 
-from dash import Dash, html, Output, Input
+from dash import Dash, html, Output, Input, dash_table
 import dash_bootstrap_components as dbc
 
 import dash_leaflet as dl
@@ -9,9 +9,43 @@ import dash_leaflet.express as dlx
 from dash_extensions.javascript import arrow_function, assign
 import plotly.figure_factory as ff
 
+
 import json
 
 from matplotlib import cm, colors
+
+month_dict = {
+    1 : 'January',
+    2 : 'February',
+    3 : 'March',
+    4 : 'April',
+    5 : 'May',
+    6 : 'June',
+    7 : 'July',
+    8 : 'August',
+    9 : 'September',
+    10 : 'October',
+    11 : 'November',
+    12 : 'December'
+}
+
+def make_table(df,month_dict):
+    df['month_name'] = df['month'].map(month_dict)
+    month = df['month_name'].unique()[0]
+    boroughs = df.groupby('borough')['average_road_speed'].mean().reset_index(name='raw_speed')
+    whole_city = df['average_road_speed'].mean()
+    table = pd.DataFrame({'borough': 'NYC Whole', 'raw_speed': whole_city}, index=[0])
+    boroughs = pd.concat([boroughs, table], ignore_index=True)
+    boroughs[f'{month}_avg_speed'] = boroughs['raw_speed'].round(1)
+    return boroughs
+
+def join_tables(df1,df2,month_dict):
+    tb1 = make_table(df1,month_dict)
+    tb2 = make_table(df2,month_dict)
+    tables = tb1.merge(tb2,on='borough')
+    drop_cols = [col for col in tables.columns if 'raw_speed' in col]
+    tables.drop(columns=drop_cols,inplace=True)
+    return tables
 
 style_handle = assign("""function(feature, context){
     const {classes, colorscale, style, colorProp} = context.hideout;  // get props from hideout
@@ -41,6 +75,13 @@ def get_info(feature=None):
 ntas = gpd.read_file("shapefiles/nynta2020_24d")
 df = pd.read_csv('static_data/rolling_avg.csv')
 
+with open("shapefiles/segments.geojson", "r") as f:
+    bus_geojson = json.load(f)
+
+oct_speed = pd.read_csv('static_data/raw_oct_speed.csv')
+nov_speed = pd.read_csv('static_data/raw_nov_speed.csv')
+
+
 rolling_df = produce_rolling(df,3,1)
 most_recent_df = rolling_df.loc[rolling_df.groupby('NTAName')['date'].idxmax()]
 rolling_map = ntas.merge(most_recent_df, left_on='NTAName', right_on='NTAName')
@@ -58,7 +99,9 @@ style = dict(weight=2, opacity=1, color='white', dashArray='3', fillOpacity=0.7)
 ctg = ["{}+".format(cls, classes[i + 1]) for i, cls in enumerate(classes[:-1])] + ["{}+".format(classes[-1])]
 colorbar = dlx.categorical_colorbar(categories=ctg, colorscale=colorscale, width=300, height=30, position="bottomleft")
 
-
+table_data = join_tables(oct_speed,nov_speed,month_dict)
+table_rows = table_data.to_dict('records')
+table_columns = [{"name": col, "id": col} for col in table_data.columns]
 
 app = Dash(__name__)
 server = app.server
@@ -81,8 +124,8 @@ info = html.Div(children=get_info(), id="info", className="info",
 app.layout = html.Div([
     html.Div([
         html.H1("NYC Bus On Time Rating", style={'font-family': 'Georgia', 'padding': '10px', 'textAlign': 'center'}),
-        html.P("This map shows a rolling average of on-time arrivals for each NTA in NYC. The on-time rating is calculated as the average number of minutes each bus is off schedule in either direction."),
-        html.P("Hover over a district to see the name and on-time rating."),
+        html.P("The map below shows two metrics for bus performance in NYC. The color of the tiles represents a rolling average of on-time arrivals for each Neighborhood Tabulation Area (NTA) in NYC. The on-time rating is calculated as the average number of minutes each bus is off schedule in either direction. The green lines represent the slowest segments of bus routes in NYC. Hover over a district to see the name and on-time rating."),
+        html.P("The on-time data is collected from the MTA's Bus Time API and is scraped everyday at rush hour. Bus speeds are collected from the NYC Open Data and are updated monthly. See table below map for average speeds by borough."),
     ], style={'font-family': 'Georgia', 'padding': '10px', 'textAlign': 'center'}),
 
     html.Div([
@@ -92,8 +135,24 @@ app.layout = html.Div([
                        zoomToBounds=True, zoomToBoundsOnClick=True,
                        hoverStyle=arrow_function(dict(weight=5, color='#666', dashArray='')),
                        hideout=dict(colorscale=colorscale, classes=classes, style=style, colorProp="rolling_avg")),
+            dl.GeoJSON(data=bus_geojson,
+                       options=dict(style=dict(color="#00FF00", weight=3, opacity=0.9, dashArray="4"))),
             colorbar, info
         ], style={'width': '100%', 'height': '75vh', 'padding-bottom': '20px', 'margin-bottom': '50px'})
+    ]),
+    html.Div([
+        html.P("The table below shows the average speed of buses in each borough. The data is collected from the NYC Open Data and is updated monthly."),
+    ])
+    ,
+    html.Div([
+        html.H2("On-Time Rating Data", style={'font-family': 'Georgia', 'textAlign': 'center', 'margin-top': '20px'}),
+        dash_table.DataTable(
+            data=table_rows,
+            columns=table_columns,
+            style_table={'width': '80%', 'margin': '0 auto'},
+            style_cell={'textAlign': 'center', 'font-family': 'Georgia', 'padding': '5px'},
+            style_header={'fontWeight': 'bold', 'backgroundColor': '#f4f4f4'}
+        )
     ])
 ])
 
--- utils/sort_and_join.py
+++ utils/sort_and_join.py
@@ -2,9 +2,26 @@ import pandas as pd
 import geopandas as gpd
 from dotenv import load_dotenv
 import os
+from shapely.geometry import Point,LineString
+import json
 
 ntas = gpd.read_file("../shapefiles/nynta2020_24d")
 df = pd.read_csv('static_data/rolling_avg.csv')
+
+month_dict = {
+    1 : 'January',
+    2 : 'February',
+    3 : 'March',
+    4 : 'April',
+    5 : 'May',
+    6 : 'June',
+    7 : 'July',
+    8 : 'August',
+    9 : 'September',
+    10 : 'October',
+    11 : 'November',
+    12 : 'December'
+}
 
 def produce_rolling(df,window_size,min):
     df['date'] = pd.to_datetime(df['date'])
@@ -12,3 +29,56 @@ def produce_rolling(df,window_size,min):
     df['avg_imputed'] = df.groupby('NTAName')['avg'].transform(lambda x: x.fillna(x.mean()))
     df['rolling_avg'] = df.groupby('NTAName')['avg_imputed'].rolling(window=window_size, min_periods=min).mean().reset_index(level=0, drop=True)
     return df
+
+def extract_segment(route, stop, next_stop):
+    """
+    Extracts the segment of the route between the stop and next_stop.
+    """
+    if not route or not stop or not next_stop:
+        return None
+    
+    line_segments = list(route.geoms) if route.geom_type == 'MultiLineString' else [route]
+    
+    for segment in line_segments:
+        if segment.distance(stop) < 1e-6 and segment.distance(next_stop) < 1e-6:
+            return segment 
+            
+    
+    return LineString([stop, next_stop])
+
+def make_slow_routes(df,count,geo_stop_dict,geo_next_stop_dict):
+    route_speed = df.groupby(['route_id','timepoint_stop_name','next_timepoint_stop_name'])['average_road_speed'].mean().reset_index()
+    slowest_routes = route_speed.sort_values('average_road_speed',ascending=True).head(count)
+
+    slowest_routes['stop_geo'] = slowest_routes['timepoint_stop_name'].map(geo_stop_dict)
+    slowest_routes['next_stop_geo'] = slowest_routes['next_timepoint_stop_name'].map(geo_next_stop_dict)
+
+    return slowest_routes
+
+def make_gpd(df,bus_routes):
+    df = df.merge(bus_routes, left_on='route_id', right_on='route_id',how='left')
+    df['stop_point'] = df['stop_geo'].apply(lambda x: Point(x['coordinates']))
+    df['next_stop_point'] = df['next_stop_geo'].apply(lambda x: Point(x['coordinates']))
+    
+    df['segment_geometry'] = df.apply(lambda row: extract_segment(row['geometry'], row['stop_point'], row['next_stop_point']), axis=1)
+    short_df = df[['route_id','timepoint_stop_name','next_timepoint_stop_name','average_road_speed','segment_geometry']]
+    gdf = gpd.GeoDataFrame(short_df, geometry='segment_geometry')
+    return gdf
+
+def make_table(df,month_dict):
+    df['month_name'] = df['month'].map(month_dict)
+    month = df['month_name'].unique()[0]
+    boroughs = df.groupby('borough')['average_road_speed'].mean().reset_index(name='raw_speed')
+    whole_city = df['average_road_speed'].mean()
+    table = pd.DataFrame({'borough': 'NYC Whole', 'raw_speed': whole_city}, index=[0])
+    boroughs = pd.concat([boroughs, table], ignore_index=True)
+    boroughs[f'{month}_avg_speed'] = boroughs['raw_speed'].round(1)
+    return boroughs
+
+def join_tables(df1,df2,month_dict):
+    tb1 = make_table(df1,month_dict)
+    tb2 = make_table(df2,month_dict)
+    tables = tb1.merge(tb2,on='borough')
+    drop_cols = [col for col in tables.columns if 'raw_speed' in col]
+    tables.drop(columns=drop_cols,inplace=True)
+    return tables
