6bc74e8e7e9c8261c13ff86b90a4919084c24ce9
SamTGoodson <samtgoodson@gmail.com>
2024-12-30 17:18:13 -0500

    text change

--- app.py
+++ app.py
@@ -68,8 +68,8 @@ info = html.Div(children=get_info(), id="info", className="info",
 
 app.layout = html.Div([
     html.Div([
-        html.H1("NYC Speed and Reliability", style={'font-family': 'Georgia', 'padding': '10px', 'textAlign': 'center'}),
-        html.P("The map below shows two metrics for bus performance in NYC. The color of the tiles represents a rolling average of on-time arrivals for each Neighborhood Tabulation Area (NTA) in NYC. The on-time rating is calculated as the average number of minutes each bus is off schedule in either direction. The green lines represent the slowest segments of bus routes in NYC. Hover over a district to see the name and on-time rating."),
+        html.H1("NYC Bus Speed and Reliability", style={'font-family': 'Georgia', 'padding': '10px', 'textAlign': 'center'}),
+        html.P("The map below shows two metrics for bus performance in NYC. The color of the tiles represents a rolling average of on-time arrivals for each Neighborhood Tabulation Area (NTA) in NYC. This was calculated by averaging the absolute difference in minutes between expected arrival and actual arrival for all stops in the NTA. The on-time rating is calculated as the average number of minutes each bus is off schedule in either direction. The green lines represent the slowest segments of bus routes in NYC. Hover over a district to see the name and on-time rating."),
         html.P("The on-time data is collected from the MTA's Bus Time API and is scraped everyday at rush hour. Bus speeds are collected from the NYC Open Data and are updated monthly. See table below map for average speeds by borough."),
         html.P(f"On-time data last scraped {scrape_date}", style={'font-style': 'italic'}),
     ], style={'font-family': 'Georgia', 'padding': '10px', 'textAlign': 'center'}),
